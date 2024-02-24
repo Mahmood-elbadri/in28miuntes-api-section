4a62e08d0f8ac0cda71c19ab35dec91ea9c8c661
Mahmood-elbadri <mahmoud.elbadry357@gmail.com>
2024-02-24 23:22:00 +0200

    added spring security

--- src/main/java/com/in28minutes/springboot/fullrestapi/security/SpringSecurityConfiguration.java
+++ src/main/java/com/in28minutes/springboot/fullrestapi/security/SpringSecurityConfiguration.java
@@ -1,2 +1,17 @@
-package com.in28minutes.springboot.fullrestapi.exception;public class SpringSecurityConfiguration {
+package com.in28minutes.springboot.fullrestapi.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class SpringSecurityConfiguration {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated());
+        http.httpBasic(withDefaults());
+        return http.build();
+    }
 }
